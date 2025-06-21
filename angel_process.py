0f22301d8e4af289d09d930a97ac0097ad7e04dc
awb1232 <166480894+awb1232@users.noreply.github.com>
2025-06-21 15:40:55 +0800

    Add files via upload

---
+++ angel_process.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def deg1deg2add(deg1, deg2):
+
+    return (deg1 + deg2) % 360
+
+def deg1deg2sub(deg1, deg2):
+    """
+    求角度差，返回0-360度的差角
+
+    :param deg1:
+    :param deg2:
+    :return:
+    """
+    return (deg1 - deg2) % 360
+
+def deg1deg2sub1(deg1, deg2):
+
+    return (deg1 - deg2 + 180) % 360 - 180
+
+def rad1rad2add(rad1, rad2):
+
+    return (rad1 + rad2) % (2 * np.pi)
+
+def rad1rad2sub(rad1, rad2):
+
+    return (rad1 - rad2) % (2 * np.pi)
+
+def rad2rad2sub1(rad1, rad2):
+
+    return (rad1 - rad2 + np.pi) % (2 * np.pi) - np.pi
